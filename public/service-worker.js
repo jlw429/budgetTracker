1701fee635d25719020db7cbbae0bce8bfd01ddd
jlw429 <jlw00329@gmail.com>
2021-04-14 19:40:39 -0400

    fetch code

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,22 +42,22 @@ self.addEventListener(`activate`, (event) => {
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
+      caches.match(event.request).then((cachedResponse) => {
         if (cachedResponse) {
           return cachedResponse;
         }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
             return cache.put(event.request, response.clone()).then(() => {
               return response;
-            })
+            });
           });
-    })
-})
-}
+        });
+      })
+    );
+  }
 });
-
